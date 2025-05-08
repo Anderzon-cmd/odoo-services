f6d42b2a4d09ba2e26743596f4989e3fa3a8b64d
Anderzon Bautista <bautistaanderzon@gmail.com>
2025-05-08 17:07:24 -0400

    add search products

--- modules/inventory_services/models/receip.py
+++ modules/inventory_services/models/receip.py
@@ -11,20 +11,20 @@ class ImportPickingWizard(models.TransientModel):
     _description = 'Import Picking from File'
 
     file = fields.Binary(string="Recibo PDF", required=True)
+    file_name=fields.Char("Nombre del archivo")
 
     url=fields.Char(string="URL", required=True, help="Endpoint URL to send the file")
 
 
     def action_import_file(self):
-        print("URL:", self.url)
-        print("File:", self.file)
+        
         if not self.file or not self.url:
             raise UserError(_("Debe proporcionar el archivo, nombre del archivo y la URL."))
         
         file_content = base64.b64decode(self.file)
 
         files = {
-            'file': ('invoice.pdf', file_content)
+            'file': (self.file_name, file_content)
         }
 
         try:
@@ -32,23 +32,56 @@ class ImportPickingWizard(models.TransientModel):
 
             response.raise_for_status() 
             result=response.json()
-            
-            print(result)
-            
+        
 
-            total_items=len(result['Productos'])
-            
-            print(total_items)
+            partner=self.env['res.partner'].search([
+                ('name','=',result['Empresa']['Nombre'])
+            ],limit=1)
+
+            picking_type=self.env.ref('stock.picking_type_in')
+
+            moves_items=[]
+
+            for item in result['Productos']:
+                product=self.env['product.product'].search([
+                    ('name','=',item['Descripcion'])
+                    ],limit=1)
+                
+                if product:
+                    moves_items.append((0,0,{
+                        'name':product.name,
+                        'product_id':product.id,
+                        'product_uom':product.uom_id.id,
+                        'product_uom_qty':float(item['Cantidad']),
+                        'location_id':picking_type.default_location_src_id.id,
+                        'location_dest_id':picking_type.default_location_dest_id.id
+                    }))
+
+            # return {
+            #     'type': 'ir.actions.client',
+            #     'tag': 'display_notification',
+            #     'params': {
+            #         'title': _('Archivo enviado'),
+            #         'message': _(f'Total de items procesados : {str(total_items)} items.'),
+            #         'type': 'success',
+            #         'sticky': False,
+            #     }
+            # }
+
             
             return {
-                'type': 'ir.actions.client',
-                'tag': 'display_notification',
-                'params': {
-                    'title': _('Archivo enviado'),
-                    'message': _(f'Total de items procesados : {str(total_items)} items.'),
-                    'type': 'success',
-                    'sticky': False,
-                }
+            'type': 'ir.actions.act_window',
+            'name': 'Recepción de Inventario',
+            'res_model': 'stock.picking',
+            'view_mode': 'form',
+            'target': 'current',
+            'context': {
+                'default_partner_id': partner.id,
+                'default_picking_type_id': picking_type.id,
+                'default_origin': result['Factura']['Numero'],
+                'default_scheduled_date': fields.Datetime.now(),
+                'default_move_ids_without_package': moves_items
+            }
             }
 
         except Exception as e:
